13435bd807857458688d11d88a284cc9fb6459e5
rajesh-devops1407 <rajeshwaran22285@gmail.com>
2026-01-23 00:57:22 +0530

    commit

---
+++ Terraform/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+source = "hashicop/aws"
+version = "~>5.0"
+    }
+
+}
+  
+}
+provider "aws" {
+  region = var.aws-region
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = var.bucket_name
+
+  tag={
+    Name = "Terraforms3Bucket"
+    Environment = "Test"
+  }
+}
+resource "aws_s3_bucket_versioning" "this" {
+    bucket = aws_s3_bucket.this.id
+
+    versioning_configuration{
+        status = "Enabled"
+    }
+}
---
+++ Terraform/variable.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+    description =   "AWS region"
+    type = string
+    default = "eu-north-1"
+  
+}
+variable "bucket_name" {
+    description = "s3_bucket"
+    type = string
+    default = "project2-terraform-s3-bucket-7"
+    
+  
+}
